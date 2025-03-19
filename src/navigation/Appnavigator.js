68d55ce1db1ecb4fd81d179ce2bf0dd19ce94030
Alexander Vélez <alexandervelez123456@gmail.com>
2025-03-19 18:29:01 -0500

    Update general.
    Se hace un update general de logo y la pantalla de Home.js.

--- src/navigation/Appnavigator.js
+++ src/navigation/Appnavigator.js
@@ -3,6 +3,9 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import SplashScreen from "../screens/SplashScreen";
 import HomeScreen from "../screens/home";
+// import ExploreScreen from "../screens/Explore"; // Descomenta si tienes una pantalla "Explore"
+// import ProfileScreen from "../screens/Profile"; // Descomenta si tienes una pantalla "Profile"
+// import SettingsScreen from "../screens/Settings"; // Descomenta si tienes una pantalla "Settings"
 
 const Stack = createStackNavigator();
 
@@ -12,6 +15,9 @@ const AppNavigator = () => {
       <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        {/* <Stack.Screen name="Explore" component={ExploreScreen} /> */}
+        {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
+        {/* <Stack.Screen name="Settings" component={SettingsScreen} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
